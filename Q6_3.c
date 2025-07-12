e477a5d3a9caf41b157b19ee618e92082c3e59ab
baadshahvv <oprajapati28@gmail.com>
2025-07-12 19:39:31 +0530

    upload

---
+++ Q6_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int number, reversed = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    while (number != 0) {
+        int digit = number % 10; 
+        reversed = reversed * 10 + digit; 
+        number /= 10; 
+    }
+
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+}
